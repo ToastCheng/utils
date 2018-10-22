fe41eaa881df9838f277aa141fbd0a6f3b5cbb7b
Toast <mrtoastcheng@gmail.com>
2018-10-22 20:24:37 +0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,9 +1,9 @@
-sudo apt-get install python3-pip python-dev build-essential 
+sudo apt-get install python-pip python-dev build-essential 
 sudo pip install --upgrade pip 
 sudo pip install --upgrade virtualenv 
 
 virtualenv toast --python=`which python3`
-echo 'alias toast='. ~/toast/bin/activate'' >> ~/.bashrc
+echo "alias toast='. ~/toast/bin/activate'" >> ~/.bashrc
 . ~/.bashrc
 . ~/toast/bin/activate
 pip install numpy pandas torch scipy matplotlib 
