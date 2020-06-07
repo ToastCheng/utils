85ef40fb7959e10f5b83c7193390fd12806e5d47
ToastC <mrtoastcheng@gmail.com>
2020-06-08 07:55:59 +0800

    add vim.sh

---
+++ vim.sh
@@ -0,0 +1,6 @@
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+echo "set mouse=a" >> ~/.vimrc
+echo "inoremap qq <Esc>" >> ~/.vimrc
+echo "set relativenumber" >> ~/.vimrc
